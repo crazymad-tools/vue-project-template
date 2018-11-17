b6e50802a67c55fdd38090aed8f5f559205debed
crazy_mad <2116913961@qq.com>
2018-11-17 14:49:41 +0800

    '删除不必要的输出log'

--- src/util/index.js
+++ src/util/index.js
@@ -3,9 +3,13 @@
  * @Date: 2018/11/17 14:06
  * @Description: 工具包
  */
+
+/**
+ * 获取前端的域名
+ * @returns {RegExpMatchArray}
+ */
 export function getDomain () {
   let url = document.location.href
   let domain = url.match(/^http(s)?:\/\/[0-9a-zA-Z-]*(:[0-9]*)+/g)
-  console.log('domain:' + domain)
   return domain
 }
